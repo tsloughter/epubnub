b24bcc3b1018402422d0faac7e456114af510c51
Tristan Sloughter <tristan.sloughter@gmail.com>
2011-04-09 11:02:56 -0500

    basic examples of using the api

---
+++ src/epn_basic_examples.erl
@@ -0,0 +1,12 @@
+-module(epn_basic_examples).
+
+-compile([export_all]).
+
+publish() ->
+    epubnub:publish("hello_world", <<"hello">>).
+
+subscribe() ->
+    epubnub:subscribe("hello_world", fun(X)-> io:format("~p~n", [X]) end).
+
+history() ->
+    epubnub:history("hello_world", 10).
