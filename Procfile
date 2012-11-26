0778cf63c13b642a8edf40333138479d5581cc47
Tristan Sloughter <tristan@heroku.com>
2012-11-26 15:02:29 -0600

    move procfile and serve program to buildpack

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: source .profile.d/ruby.sh ; bundle exec ruby serve.rb -p $PORT
--- serve.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra'
-
-set :public_folder, File.dirname(__FILE__) + '/dialyzed/'
-
-get '/' do
-  send_file File.join(settings.public_folder, 'output')
-end
