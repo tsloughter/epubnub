73f5a54294a80b8aaeb8df2bf5f3039827603711
Tristan Sloughter <t@crashfast.com>
2015-01-18 22:23:42 -0600

    fix example

--- src/epn_basic_examples.erl
+++ src/epn_basic_examples.erl
@@ -3,13 +3,13 @@
 -compile([export_all]).
 
 publish() ->
-    epubnub:publish("hello_world", <<"hello">>).
+    epubnub:publish(<<"hello_world">>, <<"hello">>).
 
 subscribe() ->
-    epubnub:subscribe("hello_world", fun(X)-> io:format("~p~n", [X]) end).
+    epubnub:subscribe(<<"hello_world">>, fun(X)-> io:format("~p~n", [X]) end).
 
 history() ->
-    epubnub:history("hello_world", 10).
+    epubnub:history(<<"hello_world">>, 10).
 
 time() ->
     epubnub:time().
